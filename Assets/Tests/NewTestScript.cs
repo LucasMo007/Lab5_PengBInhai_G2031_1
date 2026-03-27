7a0de0c27ca0572bc4d4404fb506f881c6d0f180
LucasMo007 <easoncoldplay2@gmail.com>
2026-03-27 01:26:46 -0400

    lab8 finished

---
+++ Assets/Tests/NewTestScript.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+public class PlayerSetupTest
+{
+    private GameObject player;
+
+    [SetUp]
+    public void SetUp()
+    {
+        player = new GameObject("Player");
+        //player.AddComponent<Rigidbody2D>();
+        var rb = player.AddComponent<Rigidbody2D>();
+        rb.gravityScale = 0f;
+        player.AddComponent<BoxCollider2D>();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Object.DestroyImmediate(player);
+    }
+
+    [Test]
+    public void Player_Has_Rigidbody2D()
+    {
+        Assert.IsNotNull(player.GetComponent<Rigidbody2D>());
+    }
+
+    [Test]
+    public void Player_Has_Collider2D()
+    {
+        Assert.IsNotNull(player.GetComponent<Collider2D>());
+    }
+
+    [Test]
+    public void Rigidbody2D_Is_Dynamic()
+    {
+        var rb = player.GetComponent<Rigidbody2D>();
+        Assert.AreEqual(RigidbodyType2D.Dynamic, rb.bodyType);
+    }
+
+    [Test]
+    public void Gravity_Is_Disabled()
+    {
+        var rb = player.GetComponent<Rigidbody2D>();
+        Assert.AreEqual(0f, rb.gravityScale);
+    }
+
+    [Test]
+    public void Player_Has_SpriteRenderer()
+    {
+        player.AddComponent<SpriteRenderer>();
+        Assert.IsNotNull(player.GetComponent<SpriteRenderer>());
+    }
+}
