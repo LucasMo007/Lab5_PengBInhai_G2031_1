72784a854732b22edb6bb9528bbdf8d3f633ecc5
LucasMo007 <easoncoldplay2@gmail.com>
2026-03-27 13:54:27 -0400

    Finish  Lab 9

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
@@ -7,32 +10,63 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rb;
     private float moveInput;
+    private PlayerInputActions inputActions;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        inputActions = new PlayerInputActions();
+    }
+
+    void OnEnable()
+    {
+        inputActions.Player.Enable();
+
+        inputActions.Player.Move.performed += OnMove;
+        inputActions.Player.Move.canceled += OnMoveCanceled;
+
+        EnhancedTouchSupport.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputActions.Player.Move.performed -= OnMove;
+        inputActions.Player.Move.canceled -= OnMoveCanceled;
+
+        inputActions.Player.Disable();
+        EnhancedTouchSupport.Disable();
     }
 
     void Update()
     {
-        // Read left/right arrow key input (-1 for left, +1 for right, 0 for none)
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Touch.activeFingers.Count > 0)
         {
-            moveInput = -1f;
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            moveInput = 1f;
-        }
-        else
-        {
-            moveInput = 0f;
+            Vector2 touchPos = Touch.activeFingers[0].screenPosition;
+            float screenMiddle = Screen.width / 2f;
+
+            if (touchPos.x < screenMiddle)
+            {
+                moveInput = -1f;
+            }
+            else
+            {
+                moveInput = 1f;
+            }
         }
     }
 
     void FixedUpdate()
     {
-        // Move the player left/right while preserving vertical velocity (e.g., gravity/jumps)
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<float>();
+    }
+
+    private void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        moveInput = 0f;
     }
 }
